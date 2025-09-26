f67c78ffe3bd70c44e38983a1dcc933aac55408b
杨忠宇 <kane.yang@jaco.live>
2025-09-26 19:18:47 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- client/shopping-mall/src/main.ts
+++ client/shopping-mall/src/main.ts
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 import router from "./router";
 import pinia from "./store";
+import service from "./api/request.ts";
 
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+app.use(service)
 app.mount('#app')
